6b27066fc18646d31a4bed44639d7871e1399bf6
suker <notojan19@gmail.com>
2025-08-08 16:25:29 +0900

    first commit

--- src/components/mvpblocks/feature-2.tsx
+++ src/components/mvpblocks/feature-2.tsx
@@ -67,10 +67,10 @@ export default function FeatureSteps() {
         <div className="relative mx-auto mb-12 max-w-2xl sm:text-center">
           <div className="relative z-10">
             <h2 className="font-geist text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl">
-              Build Your MVP in Three Steps
+              お庭と暮らしに、プロの手を
             </h2>
             <p className="font-geist text-foreground/60 mt-3">
-              お庭と暮らしに、プロの手を。 家まわり、まるごとおまかせ！
+              家まわり、まるごとおまかせ！
               草刈りから庭の手入れまで、暮らしの外回りお助け隊
             </p>
           </div>
