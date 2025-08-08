b0cda8078133776f8431277fb398618519379924
suker <notojan19@gmail.com>
2025-08-08 15:41:19 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Feature1 from "@/components/mvpblocks/feature-1";
+import MasonryGallery from "@/components/mvpblocks/masonry-grid-1";
+import TextRevealLetters from "@/components/mvpblocks/text-reveal-1";
+
+export default function Home() {
+  return (
+    <div>
+      <Feature1 />
+      <MasonryGallery />
+      {/* Add more components or content as needed */}
+    </div>
+  );
+}
