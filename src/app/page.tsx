1836e5a9de0318112ea5d964ae823940d81b613a
suker <notojan19@gmail.com>
2025-08-09 23:16:18 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { LampDemo } from "@/components/Lamp";
+import { LoginForm } from "@/components/login-form";
 import CTA1 from "@/components/mvpblocks/cta-1";
 import Feature1 from "@/components/mvpblocks/feature-1";
 import FeatureSteps from "@/components/mvpblocks/feature-2";
 import MasonryGallery from "@/components/mvpblocks/masonry-grid-1";
 import TextRevealLetters from "@/components/mvpblocks/text-reveal-1";
+import TypewriterDemo from "@/components/mvpblocks/typewriter-demo";
+import { ShuffleHero } from "@/components/shuffle-grid";
+import { SpinningLogos } from "@/components/spinning-logos";
 
 export default function Home() {
   return (
     <div>
-      <LampDemo />
-      <FeatureSteps />
+      <ShuffleHero />
+
       <CTA1 />
     </div>
   );
